5cb24d5342d33c289482d589c0d34b143fe52251
Quentin Devauchelle <quentin@devauchelle.eu>
2014-03-13 01:38:57 -0700

    Does not geocode if the position is already set

--- static/js/backbone/collections/shootings_collection.js
+++ static/js/backbone/collections/shootings_collection.js
@@ -1,40 +1,41 @@
 ShootingCollection = Backbone.Collection.extend({
-  model: Shooting,
-  url: '/shootings',
-  filteredShootings: null,
-  titles: null,
-  geocoder: new google.maps.Geocoder(),
+    model: Shooting,
+    url: '/shootings',
+    filteredShootings: null,
+    titles: null,
+    geocoder: new google.maps.Geocoder(),
 
-  initialize: function(){
-    this.listenTo(this, 'filter', this.byMovie);
-  },
+    initialize: function() {
+        this.listenTo(this, 'filter', this.byMovie);
+    },
 
-  byMovie: function(movie) {
+    byMovie: function(movie) {
 
-    movie = movie.toLowerCase();
-    var filtered = this.filter( function(shooting) {
-      var title = shooting.get("title").toLowerCase();
-      return(title.indexOf(movie) !== -1);
-    });
+        movie = movie.toLowerCase();
+        var filtered = this.filter(function(shooting) {
+            var title = shooting.get("title").toLowerCase();
+            return (title.indexOf(movie) !== -1);
+        });
 
-    _.each(filtered, function(shooting){
-      shooting.setGeocode();
-    });
+        _.each(filtered, function(shooting) {
+            if (shooting.position === null) {
+                shooting.setGeocode();
+            }
+        });
 
-    this.filteredShootings = new ShootingCollection(filtered);
+        this.filteredShootings = new ShootingCollection(filtered);
 
 
-    this.trigger('filtered', this.filteredShootings.models);
-  },
+        this.trigger('filtered', this.filteredShootings.models);
+    },
 
-  setTitles: function(){
-    var titles = [];
-    _.each(this.models, function(shooting){
-      titles.push(shooting.get("title"));
-    });
-    this.titles = _.uniq(titles);
-  }
+    setTitles: function() {
+        var titles = [];
+        _.each(this.models, function(shooting) {
+            titles.push(shooting.get("title"));
+        });
+        this.titles = _.uniq(titles);
+    }
 
 
 });
-
