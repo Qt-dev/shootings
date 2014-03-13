509eb5bad5c25ddb217a8191eff3ba12af9f7145
Quentin Devauchelle <quentin@devauchelle.eu>
2014-03-13 01:32:09 -0700

    Add styling for the infowindows

--- static/js/backbone/views/pin_view.js
+++ static/js/backbone/views/pin_view.js
@@ -1,3 +1,24 @@
+// An object to create objects for the pin's infowindow
+HtmlGenerator = {};
+HtmlGenerator.generateTitle = function(title, year) {
+    return ('<h4>' + title + ' (' + year + ')</h4>');
+};
+HtmlGenerator.generateDescription = function(locations, director) {
+    return (
+        "<br><p><strong>Director:</strong> " + director + "</p><p><strong>Location:</strong> " + locations + "</p><br>"
+    );
+};
+HtmlGenerator.generateFooter = function(title, year) {
+    return ("<a href='http://www.imdb.com/find?q=" + title + '(' + year + ")&s=all'>Check the movie on IMDB</a>");
+};
+
+HtmlGenerator.generateText = function(model) {
+    var attributes = model.attributes;
+    var text = this.generateTitle(attributes.title, attributes.release_year) + this.generateDescription(attributes.locations, attributes.director) + this.generateFooter(attributes.title, attributes.release_year);
+    return ('<div id="infowindow">' + text + '</div>');
+};
+
+// The pin view
 PinView = Backbone.View.extend({
     initialize: function(options) {
         var self = this;
@@ -11,11 +32,12 @@ PinView = Backbone.View.extend({
             title: title
         });
 
-        var description = options.model.attributes.title + '(' + options.model.attributes.release_year + ')\n\n' + options.model.attributes.locations;
+        var description = HtmlGenerator.generateText(options.model);
 
         // marker's popup
         this.infowindow = new google.maps.InfoWindow({
-            content: description
+            content: description,
+            maxWidth: 200
         });
 
         google.maps.event.addListener(this.marker, 'click', function() {
